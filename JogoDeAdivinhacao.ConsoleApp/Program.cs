320dc11a7c2a841ffccd2e57266cc986cc35f552
Victor <victor_hugo_moreira@hotmail.com>
2026-03-19 19:52:19 -0300

    Adicionado fluxo de input/output

--- JogoDeAdivinhacao.ConsoleApp/Program.cs
+++ JogoDeAdivinhacao.ConsoleApp/Program.cs
@@ -1,3 +1,28 @@
-﻿Console.WriteLine("Jogo De Adivinhacão");
+﻿/* 
+
+v1
+
+Iremos fazer um jogo onde o usuário terá chances de acertar um número aleatório decidido pelo sistema.
+
+Input (Entrada de Dados)O usuário digita número inteiro
+
+Processamento
+O sistema compara o número digitado com um número inteiro aleatório
+
+Output (Saída de Dados)
+O sistema informará o usuário se o mesmo acertou ou não, podendo incluir dicas sobre a proximidade do "chute"
+
+
+*/
+
+Console.WriteLine("-------------------");
+Console.WriteLine("Jogo De Adivinhacão");
 Console.WriteLine("-------------------");
 
+Console.WriteLine();
+Console.WriteLine("Comece Digitando um numero: ");
+string strNumeroDigitado = Console.ReadLine();
+
+Console.WriteLine("O Número digitado foi: " + strNumeroDigitado);
+
+Console.ReadKey();
